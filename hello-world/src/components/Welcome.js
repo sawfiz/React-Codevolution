62099e940fa111eb279c4132dce25f9edf8f491b
sawfiz <dashu.up@gmail.com>
2023-04-26 17:36:22 +0800

    10 State

--- hello-world/src/components/Welcome.js
+++ hello-world/src/components/Welcome.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 export class Welcome extends Component {
   render() {
-    return <h1>Welcome {this.props.name} aka {this.props.heroName}</h1>;
+    return (
+      <h1>
+        Welcome {this.props.name} aka {this.props.heroName}
+      </h1>
+    );
   }
 }
 
