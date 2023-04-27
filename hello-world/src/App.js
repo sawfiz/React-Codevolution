2e24b44bb1603862bea257ab139ef1b5e02b174a
sawfiz <dashu.up@gmail.com>
2023-04-27 09:23:31 +0800

    14 Event handler binding in class component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -7,12 +7,14 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './EventBind';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick />
-      <ClassClick />
+      <EventBind />
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
       {/* <Counter /> */}
       {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman">
