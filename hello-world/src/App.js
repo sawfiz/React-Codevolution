19fb84f32e7fc79726476bcb8a7d074e362a8b43
sawfiz <dashu.up@gmail.com>
2023-04-27 11:17:19 +0800

    15 methods at props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,11 +8,13 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponent />
+      {/* <EventBind /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
       {/* <Counter /> */}
